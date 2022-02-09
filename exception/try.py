0aa6a68f7b6d4ad2cd110ab83272dc1cef417b36
bibek977 <bhattaraibibek91@gmail.com>
2022-02-09 10:42:10 +0545

    exception added

---
+++ exception/try.py
@@ -0,0 +1,24 @@
+import random
+print("From 0 -9")
+rand= random.randint(0,10)
+
+while(True):
+    print("Type q or Q to quit")
+    num = input("Enter a number : ")
+    if(num=='q' or num=='Q'):
+        break
+    try:
+        n=int(num)
+        if(n==rand):
+            print("Right Guess")
+            break
+        elif(n<rand):
+            print("Guess Higher")
+        elif(n>rand):
+            print("Guess Lower")
+
+    except Exception as e:
+        print(f"Error : {e}")
+
+print("Thanks")
+
